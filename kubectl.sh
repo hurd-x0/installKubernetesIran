d5260d6d1949b5d83835d125bc54fcf928e840f8
Daniel Gordi <danial@gordi.ir>
2018-10-29 18:38:59 +0330

    Initial Commit

---
+++ kubectl.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Author: Daniel Gordi (danitfk)
+# Date: 29/Oct/2018
+
+PROXY_SERVER_ADDR="lab.gordi.ir"
+PROXY_SERVER_PORT="60666"
+function set_proxy {
+echo "Detect IP Address"
+HTTP_IP=`curl -s http://ifconfig.me"`
+echo "IP Address is: $HTTP_IP"
+export http_proxy="http://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
+export https_proxy="https://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
+echo "Detect Proxy IP"
+PROXY_IP=`host lab.gordi.ir | grep -o "address.*" | awk {'print $2'}`
+echo "Proxy IP is: $PROXY_IP"
+if [[ "$PROXY_IP" != "$HTTP_IP" ]]
+then
+	echo "Proxy not set correctly"
+	sleep 0.1
+	echo "Exit"
+	exit 1
+fi
+}
+function kube_install {
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+}
+function unset_proxy {
+export http_proxy=""
+export https_proxy=""
+}
+
+# RUN SCRIPT
+set_proxy && kube_install && unset_proxy
---
+++ minikube.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Author: Daniel Gordi (danitfk)
+# Date: 29/Oct/2018
+
+PROXY_SERVER_ADDR="lab.gordi.ir"
+PROXY_SERVER_PORT="60666"
+function set_proxy {
+echo "Detect IP Address"
+HTTP_IP=`curl -s http://ifconfig.me"`
+echo "IP Address is: $HTTP_IP"
+export http_proxy="http://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
+export https_proxy="https://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
+echo "Detect Proxy IP"
+PROXY_IP=`host lab.gordi.ir | grep -o "address.*" | awk {'print $2'}`
+echo "Proxy IP is: $PROXY_IP"
+if [[ "$PROXY_IP" != "$HTTP_IP" ]]
+then
+	echo "Proxy not set correctly"
+	sleep 0.1
+	echo "Exit"
+	exit 1
+fi
+}
+function kube_install {
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+
+}
+function unset_proxy {
+export http_proxy=""
+export https_proxy=""
+}
+
+# RUN SCRIPT
+set_proxy && kube_install && unset_proxy
