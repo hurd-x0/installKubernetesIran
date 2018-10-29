631e2cfadfa9fe860e52c4a80b54d5cf461fff45
Daniel Gordi <danial@gordi.ir>
2018-10-29 19:05:26 +0330

    Fix minikube

--- minikube.sh
+++ minikube.sh
@@ -6,7 +6,7 @@ PROXY_SERVER_ADDR="lab.gordi.ir"
 PROXY_SERVER_PORT="60666"
 function set_proxy {
 echo "Detect IP Address"
-HTTP_IP=`curl -s http://ifconfig.me"`
+HTTP_IP=`curl -s http://ifconfig.me`
 echo "IP Address is: $HTTP_IP"
 export http_proxy="http://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
 export https_proxy="http://$PROXY_SERVER_ADDR:$PROXY_SERVER_PORT"
